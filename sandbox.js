2346c7290402c154f200211f4e5c757b3318eae5
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2021-11-30 10:27:30 +0100

    added functionality and token

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,25 @@
-const { Client, Intents, Message, MessageEmbed, User } = require('discord.js');
+const { Client, Intents, Message, MessageEmbed, User, MessageAttachment } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 
-const prefix ="!";
+const prefix ="+";
+
+
+client.once('ready', () =>{
+    console.log("Conceive bot er online!")
+});
+
+client.on("messageCreate", message =>{
+    
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(" ");
+    switch (args[0]){
+        case"help":
+            message.channel.send("+help");
+        break;
+    };
+});
+
+client.login("OTE1MTYyMDEyNTQ2MzcxNTk1.YaXlCg.MvfeSSVmLdd0cKkR8Bx9As2v3zo")
