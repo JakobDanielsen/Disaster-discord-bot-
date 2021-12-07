5cfacffef5cdc5f5bee407a1866708bc9aaa1ca7
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2021-12-07 10:51:10 +0100

    YES TEST TEST noe galt

--- sandbox.js
+++ sandbox.js
@@ -53,7 +53,7 @@ client.on("messageCreate", message =>{
             message.channel.send(`You Won! New balance is ${bankBalances[message.author.id]} BTC`)
         } else {
             message.channel.send("Better luck next time!")
-            bankBalances[message.author.id] -= args[1]
+            bankBalances[message.author.id] -= args[1];
         }
     }
 
