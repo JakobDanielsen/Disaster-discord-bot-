6ef6b5372a6db46d75419712844c338698c96ccd
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2021-12-14 10:02:58 +0100

    added lots of features, like choosing what cryptocurrency you want to gamble

--- sandbox.js
+++ sandbox.js
@@ -4,6 +4,8 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 //importer json fil for å skjule token så det ikke blir resatt hver gang vi pusher botten til main
 let config = require('./config.json');
 
+let userselectedcrypto = {};
+
 const prefix ="+";
 
 // GAME VARIABLES
@@ -34,11 +36,23 @@ client.once('ready', (message) =>{
 
 
 client.on("messageCreate", message =>{
+
+
+    function selectcrypto(){
+        if(!args[1]){
+            message.channel.send("syntax: +selectCrypto [crypto]");
+        }else{
+            args[1].toUpperCase();
+            userselectedcrypto[message.author.id] = args[1];
+            message.channel.send(`you have selected ${userselectedcrypto[message.author.id]} as your form of cryptocurrency`);
+        }
+    };
+
     function getBitcoin() {
     
         if (bankBalances[message.author.id] == null ) {
             bankBalances[message.author.id] = 10;
-            message.channel.send("You were given 10 BTC")
+            message.channel.send("You were given 10: " + userselectedcrypto[message.author.id]);
         } else { 
             message.channel.send("You have already recieved your startup money")
         }
@@ -59,10 +73,10 @@ client.on("messageCreate", message =>{
             message.channel.send("You must have the amount of money you bet")
         } else if (3 == Math.round(Math.random()*3)) {
             bankBalances[message.author.id] += args[1]*3;
-            message.channel.send(`You Won! New balance is ${bankBalances[message.author.id]} BTC`)
+            message.channel.send(`You Won! New balance is ${bankBalances[message.author.id]} ${userselectedcrypto[message.author.id]} `)
         } else {
             bankBalances[message.author.id] -= args[1];
-            message.channel.send(`Better luck next time! New balance is ${bankBalances[message.author.id]} BTC`)
+            message.channel.send(`Better luck next time! New balance is ${bankBalances[message.author.id]} ${userselectedcrypto[message.author.id]} `)
         }
     }
 
@@ -83,9 +97,10 @@ client.on("messageCreate", message =>{
             let embed = new MessageEmbed()
                     .setTitle(`Disaster bot`)
                     .addFields(
-                        { name: 'getbitcoin', value: `get the bitcoin for your game :partying_face:`},
-                        { name: 'bet', value: `Bet some bitcoin!`},
-                        { name: 'bal', value: `shows you your bitcoin balance :moneybag:`},
+                        { name: 'selectcrypto', value: `select your cryptocurrency :sunglasses:`},
+                        { name: 'getcrypto', value: `get the crypto for your game :partying_face:`},
+                        { name: 'bet', value: `Bet some crypto!!`},
+                        { name: 'bal', value: `shows you your crypto balance :moneybag:`},
                     )
                     .setColor("#0099ff")
                     .setAuthor("Helene,Jakob,Szymon")
@@ -96,30 +111,34 @@ client.on("messageCreate", message =>{
         break;
 
         // GAME SECTION //
+        case"selectcrypto":
+            selectcrypto();
+        break;
 
-        case "getbitcoin":
-        case "Getbitcoin":
+        case "getcrypto":
+        case "Getcrypto":
         case "get":
         getBitcoin();
         break;
-
+        
         case "bet":
               bet();
         break;
         case "bal": 
         case"Balance": 
         case "balance":
-        message.channel.send(`You have ${bankBalances[message.author.id]} BTC`);
+        message.channel.send(`You have ${bankBalances[message.author.id]} ${userselectedcrypto[message.author.id]} `);
         break;
         case"value":
                 async function fetch(){
                 const fetch = require("node-fetch");
                 let data = await fetch(`http://api.coinlayer.com/api/live?access_key=223df91f83bcba306ce587bce6cc0fd8`).then(res =>
                 res.json()) .catch(console.error())
-                let btcvalue = Math.round(bankBalances[message.author.id] * data.rates.BTC);
+                let userdata = data.rates[userselectedcrypto[message.author.id]];
+                let btcvalue = Math.round(bankBalances[message.author.id] * userdata );
                 let embed1 = new MessageEmbed()
                     .addFields(
-                        {name:"Your bitcoin: ", value: `${thousandsSeparators(bankBalances[message.author.id])}`},
+                        {name:`Your ${userselectedcrypto[message.author.id]} : `, value: `${thousandsSeparators(bankBalances[message.author.id])}`},
                         {name: "USD equivalent: ", value: `${thousandsSeparators(btcvalue)} $`}
                     )
                     .setColor("#0099ff")
@@ -130,6 +149,26 @@ client.on("messageCreate", message =>{
         break;
         case "flip":
             flip();
+        break;
+        case "subreddit":
+            if(!args[1]){
+                message.channel.send("Syntax: +subreddit [subreddit]");
+            }else{
+                async function fetchReddit(){
+                const fetch = require("node-fetch");
+                let data = await fetch(`http://meme-api.herokuapp.com/gimme/${args[1]}`).then(res =>
+                res.json()) .catch(console.error())
+                let embed1 = new MessageEmbed()
+                    .setTitle(`here is a post from ${args[1]}`)
+                    .setURL(data.postLink)
+                    .setColor("#0099ff")
+                    .setFooter(data.ups + " Upvotes")
+                    .setTimestamp()
+                    .setImage(data.url)
+                message.channel.send({ embeds: [embed1]});
+                };
+                fetchReddit();
+            };
         break;
         default:
             message.channel.send("this is not a valid command, to see all commands type +help");
